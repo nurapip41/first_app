1748c3f56f0623e161300c379cb9d60fc33228d7
nurapip41 <nurapip@41studio.com>
2017-06-05 22:20:32 -0400

    add a login link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails51 for more book information.
 #---
 class UsersController < ApplicationController
-  
-
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+before_action :set_user, only: [:show, :edit, :update, :destroy]
   # GET /users
   # GET /users.json
   def index
